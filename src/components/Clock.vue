b7ca129555e76d14f626fa30bd23f75f0f1b192c
Prachpawee Tangpaiboon <prachpawee@nexpie.com>
2025-03-23 15:51:05 +0700

    first commit

---
+++ src/components/Clock.vue
@@ -0,0 +1,18 @@
+<script setup></script>
+
+<template>
+  <div class="clock"></div>
+</template>
+
+<style scoped>
+.clock {
+  height: 100%;
+  max-width: 70dvw; /* Maximum width is 70% of viewport width */
+  max-height: 50dvh; /* Maximum height is 50% of viewport height */
+  aspect-ratio: 1 !important; /* Ensures perfect circle */
+  margin: 8% auto 0; /* Centered with top margin */
+  border-radius: 50%; /* Circular shape */
+  position: relative;
+  border: 2px solid #000;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Clock from '@/components/Clock.vue'
+</script>
+
+<template>
+  <main>
+    home
+    <!-- clock view -->
+    <Clock />
+  </main>
+</template>
